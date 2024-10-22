15bf2c9085800b9a77e6c00328713bc58200a29b
Samantha S. <sevensasylum@gmail.com>
2024-10-22 14:29:45 -0400

    Update assignment2.js

--- Assignments/Fundamentals-1/assignment2.js
+++ Assignments/Fundamentals-1/assignment2.js
@@ -8,13 +8,13 @@ Boolean value. Also declare a variable language, but don't assign it any value y
 2. Log the types of isIsland, population, country and language to the console.
 */
 
-// const country = 'United States';
-// const continent = 'North America';
-// let population = 333;
-// const isIsland = false;
-// let language;
+const country = 'United States';
+const continent = 'North America';
+let population = 333;
+const isIsland = false;
+let language;
 
-// console.log(typeof isIsland);
-// console.log(typeof population);
-// console.log(typeof country);
-// console.log(typeof language);
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
