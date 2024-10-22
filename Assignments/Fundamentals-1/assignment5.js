3633cb6455682ebc4b578fddc4c271e6f00c559a
Samantha S. <sevensasylum@gmail.com>
2024-10-22 14:32:04 -0400

    Update assignment5.js

--- Assignments/Fundamentals-1/assignment5.js
+++ Assignments/Fundamentals-1/assignment5.js
@@ -5,5 +5,4 @@
 1. Recreate the description variable from the last assignment, this time using the template literal syntax.
  */
 
-// const description = `${country} is in ${continent}, and its ${population} million people speak
-// ${language}.`;
+ const description = `${country} is in ${continent}, and its ${population} million people speak ${language}.`;
